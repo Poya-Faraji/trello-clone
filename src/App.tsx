13f3036a5c7264278953a15158ba920ff7f50df8
Poya Faraji <poya.faraji@outlook.com>
2026-06-12 14:59:31 +0330

    [REMOVE] unnecessary code

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
-import { useState, type ReactNode } from "react";
-import Counter from "./components/Counter/Counter";
-import ThemeSwitcher from "./components/ThemeSwitcher/ThemeSwitcher";
+import { type ReactNode } from "react";
 
-export type Theme = "light" | "dark"
 
 export default function App(): ReactNode {
-  const [theme, setTheme] = useState<Theme>("dark")
 
-  const handleOnThemeToggle = (): void => {
-    setTheme(theme => theme === "light" ? "dark" : "light")
-  }
 
   return (
     <div className="app">
-      <ThemeSwitcher theme={theme} onToggleTheme={handleOnThemeToggle} />
-      <Counter primary title="first counter" theme={theme} >
-        <p>This is test children/prop for counter 1</p>
-        <ThemeSwitcher theme={theme} onToggleTheme={handleOnThemeToggle} />
-      </Counter>
-      <Counter title="second counter" theme={theme} >
-        <p>This is test children/prop for counter 2</p>
-      </Counter>
+      Hello, world!
     </div>
   )
 }
