4767ace3f738e017826c39cac3e0263a410028a8
Poya Faraji <poya.faraji@outlook.com>
2026-06-18 20:07:02 +0330

    [ADD] list-data type and simple deletion replacement

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -9,63 +9,18 @@ import IconsButton from "../IconButton/IconsButton";
 import List from "../List/List";
 
 import styles from "./Board.module.css";
+import { listsData } from "@/data/list-data";
 
 export default function Board(): ReactNode {
-  const [toDoList, setToDoList] = useState<ListType>({
-    id: crypto.randomUUID(),
-    title: "🔜 To Do",
-    items: [
-      { id: crypto.randomUUID(), title: "C First action 1" },
-      { id: "2", title: "A First action 2" },
-      { id: crypto.randomUUID(), title: "B First action 3" },
-    ],
-  });
+  const [lists, setLists] = useState<ListType[]>(listsData)
 
-  const [doingList, setDoingList] = useState<ListType>({
-    id: crypto.randomUUID(),
-    title: "🔨 Doing",
-    items: [
-      { id: crypto.randomUUID(), title: "C Doing action 1" },
-      { id: crypto.randomUUID(), title: "A Doing action 2" },
-      { id: crypto.randomUUID(), title: "B Doing action 3" },
-    ],
-  });
+  console.log(lists);
 
-  const [doneList, setDoneList] = useState<ListType>({
-    id: crypto.randomUUID(),
-    title: "✅ Done",
-    items: [
-      { id: crypto.randomUUID(), title: "C Done action 1" },
-      { id: crypto.randomUUID(), title: "ADone action 2" },
-      { id: crypto.randomUUID(), title: "B Done action 3" },
-    ],
-  });
 
-  const handleOnClickToDoList = useCallback((id: string): void => {
-    setToDoList((prev) => {
-      return { ...prev, items: prev.items.filter((item) => item.id !== id) };
-    });
-  }, []);
-
-  // smart way of using useMemo instead of useCallback however it is not necessary: 
-  // keep on using useCallback instead
-  const handleOnClickDoingList = useMemo(() => {
-    return (id: string): void => {
-      setDoingList((prev) => {
-        return {
-          ...prev,
-          items: [...prev.items].filter((item) => item.id !== id),
-        };
-      });
-    };
-  }, []);
-
-  const handleOnClickDoneList = useCallback((id: string): void => {
-    setDoneList((prev) => {
-      return {
-        ...prev,
-        items: [...prev.items].filter((item) => item.id !== id),
-      };
+  const handleOnClickItemDeletion = useCallback((id: string): void => {
+    setLists((prev) => {
+      const clone = { ...prev[0], items: [...prev[0].items].filter((item) => item.id !== id) };
+      return [clone, prev[1], prev[2]]
     });
   }, []);
 
@@ -84,15 +39,14 @@ export default function Board(): ReactNode {
       </div>
 
       <ul className={styles.lists}>
-        <li>
-          <List list={toDoList} onClick={handleOnClickToDoList} />
-        </li>
-        <li>
-          <List list={doingList} onClick={handleOnClickDoingList} />
-        </li>
-        <li>
-          <List list={doneList} onClick={handleOnClickDoneList} />
-        </li>
+
+        {lists.map(list => {
+          return (
+            <li key={list.id}>
+              <List list={list} onClick={handleOnClickItemDeletion} />
+            </li>
+          )
+        })}
       </ul>
     </div>
   );
