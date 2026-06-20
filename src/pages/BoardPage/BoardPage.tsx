ae738b901218e04d62e66c1369bbca40d8eef396
Poya Faraji <poya.faraji@outlook.com>
2026-06-20 23:13:49 +0330

    [ADD] context provider

--- src/pages/BoardPage/BoardPage.tsx
+++ src/pages/BoardPage/BoardPage.tsx
@@ -5,26 +5,16 @@ import Board from "@/components/Board/Board";
 // import { useParams } from "react-router";
 
 import styles from "./BoardPage.module.css";
-import CounterContext from "@/context/counter-context";
+import CounterProvider from "@/Provider/CounterProvider";
 
 export default function BoardPage(): ReactNode {
   // const { id } = useParams();
 
-  const [count, setCount] = useState<number>(0)
-
-  const increment = (): void => {
-    setCount(prev => prev + 1)
-  };
-
-  const reset = (): void => {
-    setCount(0)
-  };
-
   return (
     <div className={styles["board-page"]}>
-      <CounterContext value={{ count, increment, reset }}>
+      <CounterProvider>
         <Board />
-      </CounterContext>
+      </CounterProvider>
     </div>
   );
 }
