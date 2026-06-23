64a4c2e923ddf00b1c14b957be6eb2d702542528
Poya Faraji <poya.faraji@outlook.com>
2026-06-23 15:18:43 +0330

    [ADD] text input component

--- src/pages/BoardPage/BoardPage.tsx
+++ src/pages/BoardPage/BoardPage.tsx
@@ -1,11 +1,12 @@
-import { type ReactNode, useRef } from "react";
+import { type ReactNode } from "react";
 
 import BoardProvider from "@/Provider/BoardProvider";
 import ActiveItemProvider from "@/Provider/ListProvider";
 
-import Board from "@/components/Board/Board";
-import Button from "@/components/Button/Button";
-import Modal from "@/components/Modal/Modal";
+// import Board from "@/components/Board/Board";
+// import Button from "@/components/Button/Button";
+// import Modal from "@/components/Modal/Modal";
+import TextInput from "@/components/TextInput/TextInput";
 
 // import { useParams } from "react-router";
 
@@ -13,23 +14,25 @@ import styles from "./BoardPage.module.css";
 
 export default function BoardPage(): ReactNode {
   // const { id } = useParams();
-  const ref = useRef<HTMLDialogElement>(null);
+  // const ref = useRef<HTMLDialogElement>(null);
 
-  const handleOpenModalButtonClick = (): void => {
-    ref.current?.showModal();
-  };
+  // const handleOpenModalButtonClick = (): void => {
+  //   ref.current?.showModal();
+  // };
 
   return (
     <div className={styles["board-page"]}>
       <BoardProvider>
         <ActiveItemProvider>
           {/* <Board /> */}
-          <Button color="primary" onClick={handleOpenModalButtonClick}>
+          {/* <Button color="primary" onClick={handleOpenModalButtonClick}>
             open modal
           </Button>
           <Modal ref={ref} heading="This is heading">
             Hello there are children here.
-          </Modal>
+          </Modal> */}
+
+          <TextInput label="Email" />
         </ActiveItemProvider>
       </BoardProvider>
     </div>
