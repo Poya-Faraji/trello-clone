8a7d7704ca154030b3076155f7257702666daf3a
Poya Faraji <poya.faraji@outlook.com>
2026-07-16 23:11:55 +0330

    [ADD] settings page to routes

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,14 @@ import BoardPage from "@/pages/BoardPage/BoardPage.tsx";
 import HomePage from "@/pages/HomePage/HomePage.tsx";
 import NotFoundPage from "@/pages/NotFoundPage/NotFoundPage";
 
+import SettingsPage from "./pages/SettingPage/SettingsPage";
+
 export default function App(): ReactNode {
   return (
     <Routes>
       <Route element={<RootLayout />}>
         <Route index element={<HomePage />} />
+        <Route path="/settings" element={<SettingsPage />} />
         <Route path="board/:boardId" element={<BoardPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Route>
